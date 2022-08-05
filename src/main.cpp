8956c15b30d5fb65907a796fe0628ec8fb44a4e3
elseco <fernandocastanedo@hotmail.com>
2022-08-04 19:58:52 -0500

    Initial

---
+++ src/main.cpp
@@ -0,0 +1,25 @@
+#include <Arduino.h>
+#include <Servo.h>
+
+Servo srv = Servo();
+int speed = 10;
+int pos = 0;
+void setup() {
+  // put your setup code here, to run once:
+  srv.attach(9);
+  srv.write(pos);
+}
+
+void loop() {
+
+  // put your main code here, to run repeatedly:
+  for (pos = 0; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+    // in steps of 1 degree
+    srv.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(speed);                       // waits 15 ms for the servo to reach the position
+  }
+  for (pos = 180; pos >= 0; pos -= 1) { // goes from 180 degrees to 0 degrees
+    srv.write(pos);              // tell servo to go to position in variable 'pos'
+    delay(speed);                       // waits 15 ms for the servo to reach the position
+  }
+}
